e1823cac4d68200626a886adcac58c01d1aacde9
Victor E Lopez <eddie.lopezjr@gmail.com>
2020-06-17 21:18:08 +0200

    Auth0 works. Video 1 complete.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import React from "react";
+import NavBar from "./components/NavBar";
+import { Router, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import history from "./utils/history";
+import PrivateRoute from "./components/PrivateRoute";
+import DoASurvey from "./components/DoASurvey";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <PrivateRoute path="/survey" component={DoASurvey} />
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useAuth0 } from "../react-auth0-spa";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  return (
+    <div>
+      {isAuthenticated && (<button onClick={() => logout()}>Log out</button>)}
+    
+      {isAuthenticated && (
+        <span>
+          <Link to="/survey">Survey</Link>&nbsp;
+          <Link to="/profile">Profile</Link>
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default NavBar;
